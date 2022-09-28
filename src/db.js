022eaa75792cf487835e14c425056ee8b5b46a42
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2022-09-28 17:00:19 +0100

    Initial commit

---
+++ src/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const databaseServer = '127.0.0.1:27017';
+const databaseName = 'fcc-mail';
+
+
+class Database {
+    constructor() {
+        this._connect();
+    }
+
+    _connect() {
+        mongoose.connect(`mongodb:${databaseServer}/${databaseName}`).
+        then(() => {
+            console.log('Connected to database successfully.')
+        }).
+        catch(err => {
+            console.error('Database connection error.');
+        })
+    }
+}
+
+
+
+module.exports = new Database();
